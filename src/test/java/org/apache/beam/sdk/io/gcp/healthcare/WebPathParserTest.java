93608db76a00ad95d38793fd5936097cb299fef7
Aliraza Nagamia <aliraza.n@live.com>
2020-12-02 19:36:01 +0000

    Additional tests for webpath parser

--- src/test/java/org/apache/beam/sdk/io/gcp/healthcare/WebPathParserTest.java
+++ src/test/java/org/apache/beam/sdk/io/gcp/healthcare/WebPathParserTest.java
@@ -1,6 +1,7 @@
 package org.apache.beam.sdk.io.gcp.healthcare;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
@@ -9,13 +10,18 @@ import java.io.IOException;
 
 @RunWith(JUnit4.class)
 public class WebPathParserTest {
+    private WebPathParser webPathParser;
+
+    @Before
+    public void createWebParser() {
+        webPathParser = new WebPathParser();
+    }
 
     @Test
-    public void test_parsedAllElements() throws IOException {
+    public void test_parsedAllElements() throws IllegalArgumentException {
         String webpathStr = "projects/foo/location/earth/datasets/bar/dicomStores/fee/dicomWeb/studies/abc/series/xyz/instances/123";
 
-        WebPathParser parser = new WebPathParser();
-        WebPathParser.DicomWebPath dicomWebPath = parser.parseDicomWebpath(webpathStr);
+        WebPathParser.DicomWebPath dicomWebPath = webPathParser.parseDicomWebpath(webpathStr);
 
         Assert.assertNotNull(dicomWebPath);
         Assert.assertEquals("foo", dicomWebPath.project);
@@ -26,5 +32,28 @@ public class WebPathParserTest {
         Assert.assertEquals("xyz", dicomWebPath.seriesId);
         Assert.assertEquals("123", dicomWebPath.instanceId);
         Assert.assertEquals("projects/foo/location/earth/datasets/bar/dicomStores/fee", dicomWebPath.dicomStorePath);
+    }
+
+    @Test
+    public void test_nonDicomWebpath() {
+        String webpathStr = "foo/notADicomStore/bar";
+
+        try {
+            webPathParser.parseDicomWebpath(webpathStr);
+            throw new AssertionError("WebPathParser incorrectly did not throw error");
+        } catch (IllegalArgumentException e) {
+            //
+        }
+    }
+
+    @Test
+    public void test_webPathTooLong() {
+        String webpathStr = "projects/foo/location/earth/mars/datasets/bar/bam/" +
+                "dicomStores/fee/dicomWeb/studies/abc/efg/series/xyz/instances/123/";
+
+        WebPathParser.DicomWebPath dicomWebPath = webPathParser.parseDicomWebpath(webpathStr);
+
+        Assert.assertNotEquals("bar", dicomWebPath.dataset);
+        Assert.assertNotEquals("xyz", dicomWebPath.seriesId);
     }
 }

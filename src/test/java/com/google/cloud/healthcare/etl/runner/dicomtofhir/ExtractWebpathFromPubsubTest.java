32971bb67cec04fdecce5e4c75932547e88f97cb
Aliraza Nagamia <aliraza.n@live.com>
2020-12-03 15:48:22 +0000

    Adding license

--- src/test/java/com/google/cloud/healthcare/etl/runner/dicomtofhir/ExtractWebpathFromPubsubTest.java
+++ src/test/java/com/google/cloud/healthcare/etl/runner/dicomtofhir/ExtractWebpathFromPubsubTest.java
@@ -1,7 +1,19 @@
+// Copyright {current year} Google LLC.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 package com.google.cloud.healthcare.etl.runner.dicomtofhir;
 
 import com.google.cloud.healthcare.etl.runner.dicomtofhir.DicomToFhirStreamingRunner.ExtractWebpathFromPubsub;
-import org.apache.beam.sdk.Pipeline;
 import org.apache.beam.sdk.io.gcp.pubsub.PubsubMessage;
 import org.apache.beam.sdk.testing.PAssert;
 import org.apache.beam.sdk.testing.TestPipeline;
